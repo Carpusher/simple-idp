4069d02239fcb4f0fbeb1240183a9bedf9ab39f3
Carpusher <carpusher@gmail.com>
2023-05-17 23:14:00 +0800

    feat: Add navbar and sign-out in user profile

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,4 +1,10 @@
-import logo from './logo.svg';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import React, {useState} from 'react';
+import Button from 'react-bootstrap/Button';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+
 import './App.css';
 
 /**
@@ -6,21 +12,43 @@ import './App.css';
  * @return {any} main app
  */
 function App() {
+  const [showOffcanvas, setShowOffcanvas] = useState(false);
+  const handleShowOffcanvas = () => setShowOffcanvas(true);
+  const handleCloseOffcanvas = () => setShowOffcanvas(false);
+
   return (
     <div className="App">
+      <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+        <Container>
+          <Navbar.Brand href="#simple-dashboard">Simple-IDP</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#user-db-dashboard">
+                User Database Dashboard
+              </Nav.Link>
+            </Nav>
+            <Nav>
+              <Button variant="light" onClick={handleShowOffcanvas}>
+                User Profile
+              </Button>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Offcanvas
+        show={showOffcanvas}
+        onHide={handleCloseOffcanvas}
+        placement="end"
+      >
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>User Profile</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          <Button variant="outline-warning" href="sign-out">Sign out</Button>
+        </Offcanvas.Body>
+      </Offcanvas>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
